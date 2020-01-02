c9cca428293cd435fdb6be701939d5445a3fd494
educatalan02 <educatalan02@gmail.com>
2020-01-02 18:37:21 +0100

    Added

---
+++ RedoxMod-PluginExample/RedoxMod-PluginExample/Main.cs
@@ -0,0 +1,38 @@
+﻿using Redox.API.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedoxMod_PluginExample
+{
+    public class Main : RedoxPlugin
+    {
+        public override string Title => "Example Plugin";
+        public override string Description => "This is a Example plugin for Redox Mod (Unturned)";
+        public override string Author => "ice cold";
+        public override string Version => "1.0.0.0";
+
+
+        public TestCommands commands;
+        //On plugin load
+        public override void Load()
+        {
+            Logger.Log("Example plugin loaded correctly!");
+            //Register commands
+            Commands.Register("NotAdminYet", "Giving admin to everyone", Redox.API.Commands.CommandFlags.Player, commands.NotAdminYet);
+            Commands.Register("AnotherCommand", "Testing logs", Redox.API.Commands.CommandFlags.Player, commands.AnotherCommand);
+
+
+
+        }
+
+
+        //On plugin unload
+        public override void Unload()
+        {
+            Logger.Log("Unloaded plugin");
+        }
+    }
+}
---
+++ RedoxMod-PluginExample/RedoxMod-PluginExample/TestCommands.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Redox.API.Commands;
+using Redox.API.Player;
+using Redox.Unturned.Player;
+using SDG.Unturned;
+using Steamworks;
+using UnityEngine;
+using Log = Redox.Unturned.Logger;
+
+
+namespace RedoxMod_PluginExample
+{
+    public class TestCommands
+    {
+        public Redox.Unturned.Logger log;
+        public void NotAdminYet(CommandExecutor executor, string[] args)
+        {
+            var player = ((IPlayer)executor.User).Object as UnturnedPlayer;
+
+            if(!(player.IsAdmin))
+            {
+                player.Message("Due you're not admin, I gived you It");
+                player.IsAdmin = true;
+            }
+            else
+            {
+                player.Message("You're already an admin.");
+                return;
+            }
+        }
+
+
+        public void AnotherCommand(CommandExecutor playerexecutor, string[] arguments)
+        {
+            foreach(SteamPlayer player in Provider.clients)
+            {
+                foreach(string text in arguments)
+                {
+                    //Testing logs
+                    log.Log(text);
+                    ChatManager.serverSendMessage(text, Color.white, null, player, EChatMode.SAY, null, true);
+                }
+            }
+        }
+    }
+}
